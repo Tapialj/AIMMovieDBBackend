64b00dc7805ad062a97c31f0274767534cbceccd
Tapialj <ljptapia@gmail.com>
2026-07-01 21:00:05 -0500

    add lombok and update id sequencing

--- src/main/java/com/aim/capstone/model/Genre.java
+++ src/main/java/com/aim/capstone/model/Genre.java
@@ -5,74 +5,33 @@ import jakarta.persistence.*;
 
 import com.fasterxml.jackson.annotation.*;
 
+import lombok.*;
 
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "genres")
-public class Genre 
+public class Genre implements Comparable<Genre>
 {
   
   @Id
-  @SequenceGenerator(name = "genre_sequence", sequenceName = "genre_sequence", allocationSize = 1)
-  @GeneratedValue(strategy = GenerationType.AUTO, generator = "genre_sequence")
-  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "genre_id")
   private Long id;
   @Column(name = "genre")
   private String genre;
   @OneToMany(mappedBy = "genre")//, fetch = FetchType.LAZY)
   @JsonIgnore
   private List<Movie> movies;
-  
-  
-  public Genre()
-  {
-  
-  }
 
-  public Genre(String genre)
-  {
-    this.genre = genre;
-  }
-
-  public Genre(Long id, String genre)
-  {
-    this.id = id;
-    this.genre = genre;
-  }
-
-  public Long getId()
-  {
-    return id;
-  }
-
-  public void setId(Long id)
-  {
-    this.id = id;
-  }
-
-  public String getGenre()
-  {
-    return genre;
-  }
-
-  public void setGenre(String genre)
-  {
-    this.genre = genre;
-  }
-
-  public List<Movie> getMovies()
-  {
-    return movies;
-  }
-
-  public void setMovies(List<Movie> movies)
-  {
-    this.movies = movies;
-  }
 
   @Override
-  public String toString()
+  public int compareTo(Genre g)
   {
-    return "Genre [genre=" + genre + ", id=" + id + ", movies=" + movies + "]";
+    return this.getId().compareTo(g.getId());
   }
 
 }
