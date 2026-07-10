c9a5f3c66fa56fc3bfa27b8ca84de82c110f829d
Tapialj <ljptapia@gmail.com>
2026-07-10 17:06:37 -0500

    add exists by username

--- src/main/java/com/aim/capstone/repository/UserRepository.java
+++ src/main/java/com/aim/capstone/repository/UserRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>
 {
+ 
+  boolean existsByUsername(String username);
   
   Optional<User> findByUsername(String username);
 
