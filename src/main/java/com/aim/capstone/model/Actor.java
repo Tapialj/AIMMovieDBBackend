8d7aa0a52d1a3d04da3f8be41898f08868327eef
Tapialj <ljptapia@gmail.com>
2026-07-16 15:13:33 -0500

    update to use Sets and added helper functions

--- src/main/java/com/aim/capstone/model/Actor.java
+++ src/main/java/com/aim/capstone/model/Actor.java
@@ -9,10 +9,12 @@ import com.fasterxml.jackson.annotation.*;
 import lombok.*;
 
 
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "actors")
 public class Actor implements Comparable<Actor>
@@ -26,12 +28,33 @@ public class Actor implements Comparable<Actor>
   private String lastName;
   @Column(name = "first_name")
   private String firstName;
-  @ManyToMany(mappedBy = "actors")//, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+  @ManyToMany(mappedBy = "actors") //, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
   @JsonIgnore
-  private List<Movie> movies;
+  @Builder.Default
+  @EqualsAndHashCode.Exclude
+  private Set<Movie> movies = new HashSet<Movie>();
   // @OneToMany(mappedBy = "actor", cascade = CascadeType.ALL)
   // private List<Comment> comments;
 
+
+  public void addMovie(Movie movie)
+  {
+    this.movies.add(movie);
+  }
+
+  public void removeMovie(Movie movie)
+  {
+    this.movies.add(movie);
+  }
+
+  @PreRemove
+  private void removeActorFromMovies()
+  {
+    for (Movie m : this.movies)
+    {
+      m.removeActor(this);
+    }
+  }
 
   @Override
   public int compareTo(Actor a)
